fe599ce6c460b50f3e1f37228f7b970f1a9da5e2
prajwal-naik <naikprajwal40@gmail.com>
2021-10-16 22:14:50 +0530

    feed start

---
+++ components/Feed.js
@@ -0,0 +1,22 @@
+import Stories from "./Stories"
+
+function Feed() {
+    return (
+        <main className = "grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+            {/* Section */}
+            <section className = "col-span-2">
+                {/* Stories */}
+                <Stories />
+                {/* Posts */}
+            </section>
+                
+            {/* Section */}
+            <section>
+                {/* Mini profile */}
+                {/* Suggestions */}
+            </section>
+        </main>
+    )
+}
+
+export default Feed
