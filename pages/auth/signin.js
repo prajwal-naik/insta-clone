a3dc223ea467720f7445793abaeb1647debb7247
prajwal-naik <naikprajwal40@gmail.com>
2021-10-19 12:19:44 +0530

    next auth error

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,63 +1,36 @@
-// import { getProviders, signIn as signIntoProvider } from "next-auth/react";
+import { getProviders, signIn as signIntoProvider } from "next-auth/react";
 
-// function signIn({ providers }) {
+function signIn({ providers }) {
 
-//     console.log("Providers: ", providers);
+    console.log("Providers: ", providers);
 
-//     return (
-//         <>
-//             {
+    return (
+        <>
+            {
                 
-//                 Object.values(providers).map((provider) => (
-//                     <div key={provider.name}>
-//                         <button onClick={() => signIntoProvider(provider.id)}>
-//                             Sign in with {provider.name}
-//                         </button>
-//                     </div>
-//                 ))
-//             }
-//         </>
-//     )
-// }
-
-// export async function getServerSideProps(){
-//     const providers = await getProviders();
-
-//     return {
-//         props: {
-//             providers
-//         },
-//     };
-// }
-
-
-
-import React from "react";
-import { providers as getProviders, signIn } from "next-auth/react";
-
-function signin({ providers }) {
-
-    
-
-  return (
-    <>
-      {
-      Object.values(providers).map((provider) => (
-          
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
-    </>
-  );
-};
-
-export async function getInitalProps(context) {
-  return {
-    providers: await getProviders(context),
-  };
+                Object.values(providers).map((provider) => (
+                    <div key={provider.name}>
+                        <button onClick={() => signIntoProvider(provider.id)}>
+                            Sign in with {provider.name}
+                        </button>
+                    </div>
+                ))
+            }
+        </>
+    )
 }
 
-export default signin;
+export async function getServerSideProps(){
+    const providers = await getProviders();
+
+    return {
+        props: {
+            providers
+        },
+    };
+}
+
+export default signIn
+
+
+
